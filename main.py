4fbc5163f1bdf6797542cddcb004f8dc46c1462d
Tatenda Kasirori <tatendakasirori1000@gmail.com>
2025-05-11 00:00:12 -0500

    now shows missed states

--- main.py
+++ main.py
@@ -14,20 +14,36 @@ writter = Writter()
 
 def find_state(answer):
     for state in DATA.state:
-        if answer.lower() == state.lower():
+        if answer == state:
             coord = (int(DATA[DATA.state == answer].x), int(DATA[DATA.state == answer].y))
             return coord
     return False
 
 
-game_on = True
-while game_on:
+guessed_states = []
+while len(guessed_states) < 50:
     answer = screen.textinput(title= f"{writter.correct}/50", prompt= "What's another state's name?")
+    answer = answer.title()
+    if answer.lower() == "exit":
+        missing_states = []
+        for state in DATA.state:
+            if state not in guessed_states:
+                missing_states.append(state)
+        mis_states_df = pd.DataFrame(missing_states,columns=["state"])
+        mis_states_csv = mis_states_df.to_csv("missed.csv")
+        break
     coord = find_state(answer)
     if coord != False:
         writter.name_state(coord,answer)
         writter.correct += 1
-       
+        if answer not in guessed_states: guessed_states.append(answer)
+
+m_states = pd.read_csv("missed.csv")
+
+for state in m_states.state:
+    coord = find_state(state)
+    if coord != False:
+            writter.name_state(coord,state,w_color = 'red')
 t.mainloop()
 
 
--- writter.py
+++ writter.py
@@ -5,9 +5,12 @@ class Writter(Turtle):
     def __init__(self):
         super().__init__()
         self.hideturtle()
+        self.speed("fastest")
         self.penup()
         self.correct = 0
         
-    def name_state(self,coord,state):
+    def name_state(self,coord,state,w_color = "black"):
+        if w_color:
+            self.color(w_color)
         self.goto(coord)
-        self.write(arg = state.capitalize())
+        self.write(arg = state.title(), font= ("Arial", 8, "bold"))
